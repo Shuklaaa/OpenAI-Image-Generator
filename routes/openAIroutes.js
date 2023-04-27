b2c0db0b6ff35955092468105256955db9829c5e
Shuklaaa <shuklaabhishek0404@gmail.com>
2023-04-27 11:04:59 +0530

    Routes explanation

--- routes/openAIroutes.js
+++ routes/openAIroutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { generateimage } = require('../controllers/openAIcontroller')
 const router = express.Router();
 
-router.post('/generateimage', generateimage);
+router.post('/generateimage', generateimage);   // sets up a route for handling POST requests to the "/generateimage" endpoint. When a POST request is received at this endpoint, the generateimage function will be executed.
 
-module.exports = router;
+module.exports = router;   //router object is exported so that it can be used in other parts of the application.
 
